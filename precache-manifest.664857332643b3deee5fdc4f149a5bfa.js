f3456d1cfdf5f01eae897d9b71133edb577748d2
Swati Dhurandhar <swati07121994@gmail.com>
2020-06-11 17:00:43 +0530

    Updates

---
+++ precache-manifest.664857332643b3deee5fdc4f149a5bfa.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c12e6634379103040ec0f128a659b9cd",
+    "url": "/react-webpage/index.html"
+  },
+  {
+    "revision": "615cea7332156fd86fbe",
+    "url": "/react-webpage/static/css/main.306343f5.chunk.css"
+  },
+  {
+    "revision": "8a448ff4f939ef731547",
+    "url": "/react-webpage/static/js/2.3425dc7a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-webpage/static/js/2.3425dc7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "615cea7332156fd86fbe",
+    "url": "/react-webpage/static/js/main.9a4bcceb.chunk.js"
+  },
+  {
+    "revision": "d6bcfef26540c55a049b",
+    "url": "/react-webpage/static/js/runtime-main.c04a67b0.js"
+  },
+  {
+    "revision": "afbcc88b5b17ee3d859de44fc7b2c2ef",
+    "url": "/react-webpage/static/media/banner.afbcc88b.jpg"
+  },
+  {
+    "revision": "e4fdbeed8b1765fc855767c0d11c68b8",
+    "url": "/react-webpage/static/media/box1.e4fdbeed.PNG"
+  },
+  {
+    "revision": "f7834299d00198081df5beec9aa29ace",
+    "url": "/react-webpage/static/media/box2.f7834299.PNG"
+  },
+  {
+    "revision": "4f51c0d1f181931fed5e2e56d02f107d",
+    "url": "/react-webpage/static/media/box3.4f51c0d1.PNG"
+  },
+  {
+    "revision": "c42f6781d5fffb5901e5367e171bd2ab",
+    "url": "/react-webpage/static/media/footer.c42f6781.jpg"
+  },
+  {
+    "revision": "6573bc19b679a38fd3347dd5bf6a974e",
+    "url": "/react-webpage/static/media/map.6573bc19.png"
+  }
+]);
